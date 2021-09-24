4a24d6f1390764b2f165208b8b93f63fe8c07cfe
John Doedaniel <skaad102@gmail.com>
2021-09-24 18:34:06 -0500

    rutas

--- lib/app/ui/routes/app_routes.dart
+++ lib/app/ui/routes/app_routes.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart' show BuildContext, Widget;
+import 'package:offside_yopal/app/ui/pages/calendar/add_cita.dart';
 import 'package:offside_yopal/app/ui/pages/calendar/calendar_page.dart';
+import 'package:offside_yopal/app/ui/pages/calendar/custom_calendario.dart';
 import 'package:offside_yopal/app/ui/pages/home/home_pages.dart';
 import 'package:offside_yopal/app/ui/pages/login/login_page.dart';
 import 'package:offside_yopal/app/ui/pages/register/widgets/register_page.dart';
@@ -14,4 +17,6 @@ Map<String, Widget Function(BuildContext)> get appRoutes => {
       Routes.REGISTER: (_) => const RegisterPage(),
       Routes.RESET_PASSWORD: (_) => const ResetPasswordPage(),
       Routes.CALENDAR: (_) => const CalendarPage(),
+      Routes.CALENDARIO: (_) => const CustomCalendario(),
+      Routes.ADD_CITA: (_) => const AddCita(),
     };
