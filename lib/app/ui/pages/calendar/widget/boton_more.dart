4fa711686078957fbfacff9590c4135902cd459a
John Doedaniel <skaad102@gmail.com>
2021-09-24 18:35:13 -0500

    botones para añadir y eliminar

---
+++ lib/app/ui/pages/calendar/widget/boton_more.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:offside_yopal/app/ui/routes/routes.dart';
+
+class BotonAgregarCita extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SpeedDial(
+      icon: Icons.more_horiz,
+      activeIcon: Icons.more,
+      children: [
+        SpeedDialChild(
+          child: const Icon(Icons.add_box_rounded),
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white,
+          label: 'Añadir',
+          onTap: () => Navigator.pushNamed(context, Routes.ADD_CITA),
+        ),
+        SpeedDialChild(
+          child: const Icon(Icons.delete_rounded),
+          backgroundColor: Colors.deepOrange,
+          foregroundColor: Colors.white,
+          label: 'Eliminar',
+          onTap: () => debugPrint('SECOND CHILD'),
+        ),
+      ],
+    );
+  }
+}
