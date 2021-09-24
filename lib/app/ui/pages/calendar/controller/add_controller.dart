43313e34bb818ed095fabc9d70d512210f3e7c67
John Doedaniel <skaad102@gmail.com>
2021-09-24 18:34:27 -0500

    envio a firebase

---
+++ lib/app/ui/pages/calendar/controller/add_controller.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:offside_yopal/app/ui/pages/calendar/model/Cita.dart';
+import 'package:http/http.dart' as http;
+
+class AddCitaController {
+  final _url = 'offside-yopal-default-rtdb.firebaseio.com';
+
+  Future<bool> addCita(Cita cita) async {
+    print('🔥🔥🔥 ${cita.toJson()}');
+    final url = Uri.https(_url, 'CalendarData.json');
+    final repose = await http.post(url, body: citaToJson(cita));
+    final decoDate = jsonDecode(repose.body);
+    print(decoDate);
+
+    return true;
+  }
+
+  Future<List<Cita>> cargarCitas() async {
+    final url = Uri.https(_url, 'CalendarData.json');
+    final List<Cita> citas = [];
+    final response = await http.get(url);
+
+    final Map<String, dynamic> decode = jsonDecode(response.body);
+
+    decode.forEach((key, value) {
+      final cita = Cita.fromJson(value);
+      cita.user = key;
+      citas.add(cita);
+    });
+    return citas;
+  }
+}
