b4e511d0a550de27340cf811f9d0e36b33d446ad
John Doedaniel <skaad102@gmail.com>
2021-09-28 17:17:16 -0500

    calandar dart2.9

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:offside_yopal/app/inject_dependencies.dart';
 import 'app/my_app.dart';
 import 'package:flutter_meedu/router.dart' as router;
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await injectDependencies();
   router.setDefaultTransition(router.Transition.fadeIn);
-  runApp(
-     const MyApp()
-  
-  
-  );
+  runApp(const MyApp());
 }
-
