5af0d4d1a80b67b37573c58f7cd439f0946106a6
John Doedaniel <skaad102@gmail.com>
2021-09-29 19:25:35 -0500

    quitar commits yreducir

--- lib/app/ui/pages/calendar/controller/fecha_controller.dart
+++ lib/app/ui/pages/calendar/controller/fecha_controller.dart
@@ -2,11 +2,14 @@ import 'package:flutter_meedu/flutter_meedu.dart';
 
 class FechaController extends SimpleNotifier {
   DateTime _fecha = DateTime.now();
+  late DateTime _subirFecha;
 
   DateTime get fecha => _fecha;
+  DateTime get fechaFrom => _subirFecha;
 
   void selFecha(DateTime fecha) {
     _fecha = fecha;
+    _subirFecha = fecha;
     notify();
   }
 }
