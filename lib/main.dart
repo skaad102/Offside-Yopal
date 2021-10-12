87d7249a9fe738b68dceb63be46001a694407393
daniel <skaad102@gmail.com>
2021-10-12 14:53:25 -0500

    solo 2.9

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:offside_yopal/app/inject_dependencies.dart';
 import 'app/my_app.dart';
-import 'package:flutter_meedu/router.dart' as router;
+import 'package:flutter_meedu/router.dart' as router; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
