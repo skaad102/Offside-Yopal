26936fdfee2df00ed3ee19d2d03468514578091d
daniel <skaad102@gmail.com>
2021-10-12 14:57:37 -0500

    quitando viejo

--- lib/app/ui/routes/app_routes.dart
+++ lib/app/ui/routes/app_routes.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart' show BuildContext, Widget;
 import 'package:offside_yopal/app/ui/pages/calendar/add_cita.dart';
-import 'package:offside_yopal/app/ui/pages/calendar/calendar_page.dart';
 import 'package:offside_yopal/app/ui/pages/calendar/custom_calendario.dart';
+import 'package:offside_yopal/app/ui/pages/calendar/new_calendar.dart';
 import 'package:offside_yopal/app/ui/pages/home/home_pages.dart';
 import 'package:offside_yopal/app/ui/pages/login/login_page.dart';
 import 'package:offside_yopal/app/ui/pages/register/widgets/register_page.dart';
@@ -16,7 +16,7 @@ Map<String, Widget Function(BuildContext)> get appRoutes => {
       Routes.HOME: (_) => const HomePage(),
       Routes.REGISTER: (_) => const RegisterPage(),
       Routes.RESET_PASSWORD: (_) => const ResetPasswordPage(),
-      Routes.CALENDAR: (_) => const CalendarPage(),
       Routes.CALENDARIO: (_) => const CustomCalendario(),
       Routes.ADD_CITA: (_) => const AddCita(),
+      Routes.NEW_CITA: (_) => const LoadDataFromFireStore(),
     };
